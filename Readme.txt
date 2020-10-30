5a7f988356766a1841fd4ac66f50e5936b14b996
KWAN YEOW CHYUAN <kwanyc95@outlook.com>
2020-10-30 23:39:44 +0800

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+1) Open ResourceGroups.csv.
+2) Add on the name of Resource Groups that you want to create.
+3) Open Execute.ps1.
+4) Change <SubscriptionID> in line 4.
+5) Change <TenantID> in line 5.
+6) Execute Execute.ps1.
+7) Login.
+8) Verify after "Finish Create all RG!".
+
+Note: Install Azure CLI if not install any Az module. (Refer to line 2).
---
+++ ResourceGroups/Execute.ps1
@@ -0,0 +1,17 @@
+﻿###Run this if not install Az module##############################################################################################################################################
+###Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+##################################################################################################################################################################################
+$Subscription = "<SubscriptionID>"
+$TenantID = "<TenantID>"
+$csv = Import-Csv .\ResourceGroups.csv
+$location = "southeastasia"
+az login --allow-no-subscriptions -t $TenantID
+az account set -s $Subscription
+foreach ($line in $csv) {
+$RG = $line.Name
+Write-Host "Creating Resource Groups " -NoNewline -ForegroundColor Red
+Write-Host $RG -NoNewline -ForegroundColor Red
+Write-Host "..." -ForegroundColor Red
+az group create -l $location -n $RG
+}
+Write-Host "Finish Create all RG!" -ForegroundColor Blue
